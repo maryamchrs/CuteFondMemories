f982805a12765648c5ee34d3742c5fe6c97349b9
Maryam Chrs <maryam.chrs@gmail.com>
2024-02-12 21:58:44 +0330

    Add AnnotaionInfoModel And Location as necessary dataModel to project

---
+++ CuteFondMemories/Modules/Dashboard/Model/AnnotaionInfoModel.swift
@@ -0,0 +1,16 @@
+//
+//  AnnotaionInfoModel.swift
+//  CuteFondMemories
+//
+//  Created by Maryam Chrs on 12/02/2024.
+//
+
+import Foundation
+
+struct AnnotaionInfoModel {
+    let title: String
+    let description: String?
+    let location: Location
+    let voice: Data?
+    let images: [Data]?
+}
---
+++ CuteFondMemories/Modules/Dashboard/Model/Location.swift
@@ -0,0 +1,14 @@
+//
+//  Location.swift
+//  CuteFondMemories
+//
+//  Created by Maryam Chrs on 12/02/2024.
+//
+
+import Foundation
+
+import MapKit
+
+struct Location {
+    var coordinate: CLLocationCoordinate2D
+}
