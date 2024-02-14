2e9ad1922ef9a0ebba11d50c6b4a961ce78a4797
Maryam Chrs <maryam.chrs@gmail.com>
2024-02-14 11:28:01 +0330

    Add UInavigationViewController to the Scene Delegate.

---
+++ CuteFondMemories/Common/Extensions/UITextViewExtension.swift
@@ -0,0 +1,8 @@
+//
+//  UITextViewExtension.swift
+//  CuteFondMemories
+//
+//  Created by Maryam Chrs on 14/02/2024.
+//
+
+import Foundation
---
+++ CuteFondMemories/Common/Extensions/UIViewExtension.swift
@@ -0,0 +1,8 @@
+//
+//  UIViewExtension.swift
+//  CuteFondMemories
+//
+//  Created by Maryam Chrs on 14/02/2024.
+//
+
+import Foundation
