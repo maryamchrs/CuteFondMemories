c8adf3dd633ee669c19db2326ffd60f6cd9c4bc8
Maryam Chrs <maryam.chrs@gmail.com>
2024-02-14 11:28:49 +0330

    Change name of extension files. and add some methods to them

--- CuteFondMemories/Common/Extensions/UITextViewExtension.swift
+++ CuteFondMemories/Common/Extensions/UITextViewExtension.swift
@@ -5,4 +5,20 @@
 //  Created by Maryam Chrs on 14/02/2024.
 //
 
-import Foundation
+import UIKit
+
+extension UITextView {
+    
+    func adjustUITextViewHeight() {
+        self.sizeToFit()
+        self.isScrollEnabled = false
+    }
+    
+    func calculateTextDescription() -> CGSize {
+        let fixedWidth = self.frame.size.width
+        self.sizeThatFits(CGSize(width: fixedWidth,
+                                 height: CGFloat.greatestFiniteMagnitude))
+        return self.sizeThatFits(CGSize(width: fixedWidth,
+                                        height: CGFloat.greatestFiniteMagnitude))
+    }
+}
