88850367e7496e719db776c99682a20d3b5a8abc
Maryam Chrs <maryam.chrs@gmail.com>
2024-02-16 21:21:29 +0330

    Add PersistanceManager and NSPredicateExtension

--- CuteFondMemories/CoreData/NSPredicateExtension.swift
+++ CuteFondMemories/CoreData/NSPredicateExtension.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+extension NSPredicate {
+    
+    static let all = NSPredicate(format: "TRUEPREDICATE")
+    
+    static let none = NSPredicate(format: "FALSEPREDICATE")
+    
+}
--- CuteFondMemories/CoreData/PersistanceManager.swift
+++ CuteFondMemories/CoreData/PersistanceManager.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import CoreData
+
+/*
+ To ensure our database remains secure, it is crucial that only one instance can access it.
+ Therefore, we must use the Singleton design pattern.
+ */
+struct PersistanceManager {
+    
+    static let shared = PersistanceManager()
+    let container: NSPersistentContainer
+    
+    private init(inMemory: Bool = false) {
+        self.container = NSPersistentContainer(name: "CuteFondMemories")
+        /*
+         That is exactly what you want for a persistent store that is in memory only.
+         It ensures that nothing will actually be persisted and each time you run the app in the simulator it will start with a fresh store.
+         */
+        if inMemory {
+            container.persistentStoreDescriptions.first?.url = URL(fileURLWithPath: "dev/null")
+        }
+        
+        container.loadPersistentStores { description, error in
+            
+            if let error = error as? NSError {
+                Logger.log(text: "error: \(error)")
+            }
+            
+        }
+    }
+}
