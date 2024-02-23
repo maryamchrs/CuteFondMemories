f3a2b62cd978357abb67bf3c186cbf9716123f10
Maryam Chrs <maryam.chrs@gmail.com>
2024-02-23 20:54:09 +0330

    Add an extension to Date to remove time from date

---
+++ CuteFondMemories/Common/Extensions/DateExtension.swift
@@ -0,0 +1,18 @@
+//
+//  DateExtension.swift
+//  CuteFondMemories
+//
+//  Created by Maryam Chrs on 23/02/2024.
+//
+
+import Foundation
+
+extension Date {
+    
+    var withoutTime: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yy"
+        return dateFormatter.string(from: self)
+    }
+    
+}
