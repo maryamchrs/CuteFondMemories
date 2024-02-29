b1c62e6172934c6add683806f2387f24c6138a46
Maryam Chrs <maryam.chrs@gmail.com>
2024-02-29 22:03:21 +0330

    Add Localizable and required methods in StringExtension

---
+++ CuteFondMemories/Common/Extensions/StringExtension.swift
@@ -0,0 +1,28 @@
+//
+//  StringExtension.swift
+//  CuteFondMemories
+//
+//  Created by Maryam Chrs on 29/02/2024.
+//
+
+import Foundation
+
+extension String {
+    
+    var localize: String {
+        return NSLocalizedString(self, tableName: "Localizable", bundle: Bundle.main, value: "", comment: "")
+    }
+    
+    func localizedWithArgs(_ args: [CVarArg]) -> String {
+        return String(format: self.localize, arguments: args)
+    }
+    
+    func localizedWithLang(_ lang: String) -> String {
+        return NSLocalizedString(self, tableName: lang, bundle: Bundle.main, value: "", comment: "")
+    }
+    
+    func localizedWithLangAndArgs(_ lang: String, _ args: [CVarArg]) -> String {
+        return String.init(format: self.localizedWithLang(lang), args)
+    }
+    
+}
