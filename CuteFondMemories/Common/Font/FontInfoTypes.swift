cfd6bf9d0385d0981c07be85bfb651df08b7d193
Maryam Chrs <maryam.chrs@gmail.com>
2024-08-28 22:53:42 +0330

    Add UIFONT extension

---
+++ CuteFondMemories/Common/Font/FontInfoTypes.swift
@@ -0,0 +1,31 @@
+//
+//  FontInfoTypes.swift
+//  CuteFondMemories
+//
+//  Created by Maryam Chrs on 28/08/2024.
+//
+
+import Foundation
+
+enum CustomFonts: String {
+    case lato = "Montserrat"
+}
+
+enum CustomFontStyle: String {
+    case bold = "-Bold"
+    case light = "-Light"
+    case medium = "-Medium"
+    case regular = "-Regular"
+    case semiBold = "-SemiBold"
+    case thin = "-Thin"
+    case italic = "-Italic"
+    case thinItalic = "-ThinItalic"
+}
+
+enum CustomFontSize: CGFloat {
+    case h0 = 36.0
+    case h1 = 32.0
+    case h2 = 20.0
+    case h3 = 16.0
+    case h4 = 8.0
+}
